9a9db56fa66b6706c4d949617df3e7f695356d3b
apexloaded <admin@apexloaded.com>
2025-03-12 17:13:18 +0100

    updated app key name

--- src/enums/cookie.enum.ts
+++ src/enums/cookie.enum.ts
@@ -1,5 +1,5 @@
 export enum Cookies {
   ACCESS_TOKEN = 'wallet.dexa.access-token',
   REFRESH_TOKEN = 'wallet.dexa.refresh-token',
-  APP_KEY = 'wallet.dexa.app-key',
+  APP_KEY = 'x-wallio-app-key',
 }
